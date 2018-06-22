0cb881a0a059e79f7c898676f7f6c1c1837ac0c8
Kevin Lazich <dev@kevinlazich.com>
2018-06-22 08:34:11 -0400

    chore: Switch encryption from bcrypt to argon2

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -1,15 +1,21 @@
-import bcrypt from 'bcrypt'
+import argon2 from 'argon2'
 
-const saltRounds = 15
+export async function makeHash(password) {
+  try {
+    const hash = await argon2.hash(password)
+  } catch (err) {
+    console.error('Internal Error!', err)
+  }
 
-export async function getHash(password) {
-  const hashed = await bcrypt.hash(password, saltRounds)
-
-  return hashed
+  return hash
 }
 
-export async function checkPassword(password, passwordHash) {
-  const match = await bcrypt.compare(password, passwordHash)
+export async function verifyPassword(hash, password) {
+  try {
+    const match = await argon2.verify(hash, password)
+  } catch (err) {
+    console.error('Internal Error!', err)
+  }
 
   return match
 }
