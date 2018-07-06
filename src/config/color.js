1c465f122ebb751331f6afca05fac7c28689f7f2
Kevin Lazich <dev@kevinlazich.com>
2018-07-06 16:18:20 -0400

    chore: Add color output

---
+++ src/config/color.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk'
+
+export const ERROR = chalk.red('error')
+export const WARNING = chalk.keyword('orange')('warning')
+export const SUCCESS = chalk.green('success')
+export const INFO = chalk.cyan('info')
+export const DEBUG = chalk.yellow('debug')
+
+export const em = chalk.blueBright
